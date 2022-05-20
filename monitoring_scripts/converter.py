b7629abdbf111ee7fdc6a477695291ce4510000d
yebh17 <sunnyrebels0@gmail.com>
2022-05-20 16:17:10 +0200

    make python executable with shebang notation

--- monitoring_scripts/converter.py
+++ monitoring_scripts/converter.py
@@ -1,4 +1,6 @@
-with open('tmp/tmp_metrics.log') as f:
+#!/usr/local/bin/python3
+
+with open('/Users/sunny/Desktop/tmp_metrics.log') as f:
     lines = f.readlines()
     print(lines)
 span = 2
@@ -6,7 +8,7 @@ words = lines[1].split(' |')
 z=[]
 z.append(lines[0])
 listt=z+[" | ".join(words[i:i+span]) for i in range(0, len(words), span)]
-textfile = open("tmp/metrics.log", "w")
+textfile = open("monitoring_scripts/metrics.log", "w")
 for element in listt:
     textfile.write(element + "\n")
 textfile.close()
