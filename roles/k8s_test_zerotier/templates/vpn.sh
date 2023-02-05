07674585b920c467e958bcb49f7b31613c43b573
yebh17 <sunnyrebels0@gmail.com>
2023-02-05 13:21:33 +0100

    Update the nginx configs and also fix the missing /etc/docker/daemon.json issue

--- roles/k8s_test_zerotier/templates/vpn.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-curl -s 'https://pgp.mit.edu/pks/lookup?op=get&search=0x1657198823E52A61' | gpg --import
-
-if z=$(curl -s 'https://install.zerotier.com/' | gpg)
-then
-	echo "$z" | sudo bash
-fi
