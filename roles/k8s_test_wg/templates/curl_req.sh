6bc8899fc18008c8f0eea7fa50c950b377c6dbec
yebh17 <sunnyrebels0@gmail.com>
2023-02-18 10:00:33 +0100

    Update curl_requests scripts with adding functions and optimizations

--- roles/k8s_test_wg/templates/curl_req.sh
+++ roles/k8s_test_wg/templates/curl_req.sh
@@ -1,47 +1,50 @@
 #!/bin/bash
+
+print_header () {
+    echo "code,time_total"
+}
+
+cleanup_logs () {
+    echo "REMOVING METRICS LOG FILE"
+    rm -f metrics.log
+    echo "REMOVING ERRORS LOG FILE"
+    rm -f errors.log
+    echo "RESPONSE_CODE | TIME_TOTAL(s) | POD_ID" >> metrics.log
+    echo "RESPONSE_CODE | TIME_TOTAL(s) | POD_ID" >> errors.log
+}
+
+make_request () {
+    local url=$1
+    local http_status=$(curl --write-out %{http_code} --silent --output /dev/null $url)
+    local response_time=$(curl --write-out %{time_total} --silent --output /dev/null $url)
+
+    if [[ "$http_status" -ne 200 ]] ; then
+        echo "$http_status,$response_time" >> errors.log
+    else
+        local pod_id=$(curl --silent $url | grep -o 'helloweb[^;]*')
+        echo "200,$response_time,$pod_id" >> metrics.log
+    fi
+}
+
+cleanup_temp_files () {
+    rm -f output.log
+}
+
 main () {
     local url=$1
     if [[ -z "$url" ]]; then
-        echo "ERROR:
-        An URL must be provided.
-        Usage: check-res <url>
-        Aborting.
-        "
+        echo "ERROR: Please provide an URL as an argument for processing the requests"
         exit 1
     fi
+
     print_header
-    echo "REMOVING METRICS LOG FILE"
-    rm -rf metrics.log
-    echo "REMOVING ERRORS LOG FILE"
-    rm -rf errors.log
-    echo "RESPONSE_CODE | TIME_TOTAL(s) | POD_ID" >> metrics.log
-    echo "RESPONSE_CODE | TIME_TOTAL(s) | POD_ID" >> errors.log
+    cleanup_logs
+
     for i in $(seq 1 10000); do
         make_request $url
     done
-    rm -rf output.log
-    }
-    print_header () {
-        echo "code,time_total"
-    }
-    make_request () {
-        local url=$1
-        http_status=$(curl --write-out %{http_code} --silent --output output.log $url)
-        if [[ "$http_status" -ne 200 ]] ; then
-            curl \
-            --write-out "%{http_code} | %{time_total} | " \
-            --silent \
-            --output output.log \
-            >> errors.log \
-            "$url"
-        else
-            curl \
-            --write-out "%{http_code} | %{time_total} | " \
-            --silent \
-            --output output.log \
-            >> metrics.log \
-            "$url"
-            curl --silent $url | grep -o 'helloweb[^;]*' >> metrics.log
-        fi
-    }
+
+    cleanup_temp_files
+}
+
 main "$@"
